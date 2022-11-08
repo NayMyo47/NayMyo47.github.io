dc42be4a428a35dcbe25b6ac41e641287b534394
NayMyo47 <boyka9001@gmail.com>
2022-11-09 00:26:25 +0630

    html

--- ForShima/shima.js
+++ ForShima/shima.js
@@ -1,12 +1,12 @@
-let shima = "shima";
-let guess = prompt('Type the password to enter this site!')
+// let shima = "shima";
+// let guess = prompt('Type the password to enter this site!')
 
-while(shima != guess){
-   if (guess != shima){
-       alert('Sorry. You type the wrong password.');
-   }
-   guess = prompt('Type the password to enter this site!')
-} 
+// while(shima != guess){
+//    if (guess != shima){
+//        alert('Sorry. You type the wrong password.');
+//    }
+//    guess = prompt('Type the password to enter this site!')
+// } 
 function myFunction(){
     var x = document.getElementsByClassName("pvDiv");
     if (x.style.display === "block") {
