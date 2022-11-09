97760d1702e957872e823dbc3ad63315b6c14bb6
NayMyo47 <boyka9001@gmail.com>
2022-11-09 11:41:49 +0630

    Forshima

--- ForShima/shima.js
+++ ForShima/shima.js
@@ -1,9 +1,19 @@
-// let shima = "shima";
-// let guess = prompt('Type the password to enter this site!')
+let shima = "shima";
+let guess = prompt('Type the password to enter this site!')
 
-// while(shima != guess){
-//    if (guess != shima){
-//        alert('Sorry. You type the wrong password.');
-//    }
-//    guess = prompt('Type the password to enter this site!')
-// } 
+while(shima != guess){
+   if (guess != shima){
+       alert('Sorry. You type the wrong password.');
+   }
+   guess = prompt('Type the password to enter this site!')
+} 
+
+function mobileMenu(){
+  let x = document.querySelector('.pvDiv');
+  let y = document.querySelector('.menu');
+  if (x.style.display === 'block'){
+      x.style.display = 'none';
+  }else {
+      x.style.display = 'block';
+  }
+}
